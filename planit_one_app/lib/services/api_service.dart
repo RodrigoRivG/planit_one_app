279863ae01a7f5579c7fe7be1342760fd8f2967c
Rodrigo <rodrigoriverogonzales@gmail.com>
2025-06-23 11:35:59 -0400

    a

--- planit_one_app/lib/services/api_service.dart
+++ planit_one_app/lib/services/api_service.dart
@@ -1,3 +1,4 @@
-const String baseUrl = 'https://web-production-afc7c.up.railway.app/api/';    //En Produccion
+//const String baseUrl = 'https://software-eventos-backend.onrender.com/api/';    //En Produccion
 //const String baseUrl = 'http://192.168.0.13:8000/api/';                  //En Local
-//https://web-production-afc7c.up.railway.app/api/  --> https://web-production-bdd16.up.railway.app/api/
+const String baseUrl = 'http://192.168.0.9:8000/api/';
+//https://web-production-afc7c.up.railway.app/api/  --> https://web-production-bdd16.up.railway.app/api/ https://software-eventos-backend.onrender.com/
